df76796eb00c3d25c19d83b5cc1b348d5f1439a1
z <z@Z>
2016-07-30 00:48:00 -0500

    sendText added, pending validation for sending

--- src/main/java/bot.java
+++ src/main/java/bot.java
@@ -1,6 +1,9 @@
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Random;
@@ -15,10 +18,15 @@ public class bot {
 	public static void main(String[] args) throws Exception {
 		bot http = new bot();
 
-		System.out.println("Enviando Get para obtener wss");
+		//System.out.println("Enviando Get para obtener wss");
 		//http.sendGet();
-		System.out.println("Intentemos conectarnos a API RTM ");
-		http.rtmConnection(sendGet());
+		//System.out.println("Intentemos conectarnos a API RTM ");
+		ByteArrayOutputStream temp = new ByteArrayOutputStream();
+		PrintStream sysin = new PrintStream(temp);
+		PrintStream old = System.out;
+		System.setOut(sysin);
+		
+		http.rtmConnection(sendGet(), temp, old);
 }
 	
 	public static String sendGet() throws Exception {
@@ -29,8 +37,9 @@ public class bot {
 		final String message = "shi";
 		final boolean asUser = true;
 		//final String coreURL = "https://slack.com/api/chat.postMessage?token="+tokenBot+"&channel="+channel+"&text="+message+"&as_user="+asUser;
+		final String tokenBot = "xoxb-50461497911-kB6Lk2JG8m6zKWdpbZyFVgYB";
 		final String startURL = "https://slack.com/api/rtm.start?token="+tokenBot+"&pretty=1";
-		final String tokenBot = "xoxb-50461497911-f7a9PF6MKBcmsoNo2GXaJOraY";
+		
 		
 		URL obj = new URL(startURL + tokenBot);
 		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
@@ -42,8 +51,8 @@ public class bot {
 		con.setRequestProperty("User-Agent", USER_AGENT);
 
 		int responseCode = con.getResponseCode();
-		System.out.println("\nSending 'GET' request to URL : " + startURL);
-		System.out.println("Response Code : " + responseCode);
+		//System.out.println("\nSending 'GET' request to URL : " + startURL);
+		//System.out.println("Response Code : " + responseCode);
 
 		BufferedReader in = new BufferedReader(
 		        new InputStreamReader(con.getInputStream()));
@@ -59,20 +68,38 @@ public class bot {
 		
 		JSONObject json = new JSONObject(response.toString());
 		String wss = json.getString("url");
-		System.out.println(wss);
+		//System.out.println(wss);
 		return wss;
 
 	}
 	
-	public void rtmConnection(String response) throws Exception{
-		final String sendMsj = "{id\": 1,\"type\": \"message\",\"channel\": \"C024BE91L\",\"text\": \"Hello world\"}";
+	public void rtmConnection(String response, ByteArrayOutputStream temp, PrintStream old) throws Exception{
+		JSONObject sendMsj = new JSONObject();
+		sendMsj.accumulate("id", "1")
+		.accumulate("type", "message")
+		.accumulate("channel", "D1GDGLD16")
+		.accumulate("text", "jojo");
 		
 		WebSocket ws = connect(response);
-					
+		//System.out.println(sendMsj.toString());
+		//System.out.flush();
+	    //System.setOut(old);
+		//System.out.println(":D" + temp.toString() + ":D");
+		while(!temp.toString().equals("")){
+		String jeje = temp.toString();
+		if(temp.toString().equals("Exito")){
+			ws.sendText(sendMsj.toString());
+			System.out.flush();
+		    System.setOut(old);
+			System.out.println("Msj sended :D");
+		}
+		}
+		
 	}
 	
 	private static WebSocket connect(String response) throws IOException, WebSocketException
     {
+		
         return new WebSocketFactory()
             .createSocket(response)
             .addListener(new WebSocketAdapter() {
@@ -80,8 +107,8 @@ public class bot {
                 public void onTextMessage(WebSocket websocket, String message) {
                 	JSONObject filter = new JSONObject(message);
                 	//System.out.println(message);
-                	System.out.println(filter.getString("channel")+"__"+filter.getString("type"));                	
-                	if(filter.getString("type")=="message" && filter.getString("channel")=="D1GDGLD16"){
+                	//System.out.println(filter.getString("channel")+"__"+filter.getString("type"));                	
+                	if(filter.getString("type").equals("message") && filter.getString("channel").equals("D1GDGLD16")){
                     System.out.println("Exito");
                 	}
                 }
